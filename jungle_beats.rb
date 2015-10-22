2ee9d6364fbe38ebe41383e659d095e5cfcfb767
GregoryArmstrong <intergalacticgentleman@gmail.com>
2015-10-21 20:35:13 -0600

    functional and finished program. still needs refactoring and ability to insert to an empty list.

--- jungle_beats.rb
+++ jungle_beats.rb
@@ -11,11 +11,11 @@ class JungleBeat
     end
   end
 
-  def create_initial_head(beat)
-    split_beat = beat.split
+  def create_initial_head(string)
+    split_string = string.split
     @head = Node.new(nil)
     current_node = @head
-    split_beat.each do |x|
+    split_string.each do |x|
       current_node.link = Node.new(x)
       current_node = current_node.link
     end
@@ -35,8 +35,7 @@ class JungleBeat
 
   def append(string)
     if @head == nil
-      @head = Node.new
-      @head.link = Node.new(string)
+      create_initial_head(string)
     else
       split_string = string.split
       split_string.each do |x|
@@ -49,5 +48,132 @@ class JungleBeat
       end
     end
   end
+
+  def prepend(string)
+    if @head == nil
+      create_initial_head(string)
+    else
+      stored_head_link = @head.link
+      create_initial_head(string)
+      find_tail.link = stored_head_link
+    end
+  end
+
+  def insert(position, string)
+    current_node = @head.link
+    (position - 1).times do
+      current_node = current_node.link
+    end
+    stored_post_insert_link = current_node.link
+    split_string = string.split
+    split_string.each do |x|
+      current_node.link = Node.new(x)
+      current_node = current_node.link
+    end
+    find_tail.link = stored_post_insert_link
+  end
+
+  def find(position, number_of_elements)
+    current_node = @head.link
+    (position).times do
+      current_node = current_node.link
+    end
+    output = []
+    (number_of_elements).times do
+      output << current_node.data
+      current_node = current_node.link
+    end
+    p output.join(" ")
+  end
+
+  def includes?(string)
+    current_node = @head.link
+    while current_node.link != nil
+      if current_node.data == string
+        return true
+      end
+      current_node = current_node.link
+    end
+    if current_node.data == string
+      return true
+    end
+  end
+
+  def count
+    if @head == nil
+      return 0
+    else
+      current_node = @head.link
+      counter = 0
+      while current_node.link != nil
+        counter += 1
+        current_node = current_node.link
+      end
+      p counter + 1
+    end
+  end
+
+  def pop(number)
+    if @head == nil
+      p "Cannot pop off any beats!"
+    else
+      position = count - number
+      current_node = @head.link
+      (position - 1).times do
+        current_node = current_node.link
+      end
+      stored_rest_of_data = current_node.link
+      current_node.link = nil
+      output = []
+      number.times do
+        output << stored_rest_of_data.data
+        stored_rest_of_data = stored_rest_of_data.link
+      end
+      p output.join(" ")
+    end
+  end
+
+  def all
+    current_node = @head.link
+    output = []
+    (count).times do
+      output << current_node.data
+      current_node = current_node.link
+    end
+    p output.join(" ")
+  end
+
+  def play
+    p `say -v Boing #{all}`
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
--- jungle_beats_test.rb
+++ jungle_beats_test.rb
@@ -6,20 +6,20 @@ require_relative './jungle_beats'  # => true
 class JungleBeatsTest < Minitest::Test
 
   def test_starts_with_no_head
-    jb = JungleBeat.new         # => #<JungleBeat:0x007fa2e290d850 @head=nil>
+    jb = JungleBeat.new         # => #<JungleBeat:0x007fa7fa2a81c8 @head=nil>
 
     assert_equal nil, jb.head  # => true
   end
 
   def test_if_given_one_string_creates_head_with_only_link
-    jb = JungleBeat.new("deep")                             # => #<JungleBeat:0x007fa2e2904638 @head=#<Node:0x007fa2e29045c0 @data=nil, @link=#<Node:0x007fa2e2904598 @data="deep", @link=nil>>>
+    jb = JungleBeat.new("deep")                             # => #<JungleBeat:0x007fa7fa2a2ef8 @head=#<Node:0x007fa7fa2a2e80 @data=nil, @link=#<Node:0x007fa7fa2a2e58 @data="deep", @link=nil>>>
 
     assert_equal "deep", jb.head.link.data  # => true
     assert_equal nil, jb.head.data          # => true
   end
 
   def test_if_given_multiple_string_creates_head_and_nodes
-    jb = JungleBeat.new("deep boop derp burp")              # => #<JungleBeat:0x007fa2e290e6d8 @head=#<Node:0x007fa2e290e5e8 @data=nil, @link=#<Node:0x007fa2e290e5c0 @data="deep", @link=#<Node:0x007fa2e290e598 @data="boop", @link=#<Node:0x007fa2e290e570 @data="derp", @link=#<Node:0x007fa2e290e548 @data="burp", @link=nil>>>>>>
+    jb = JungleBeat.new("deep boop derp burp")              # => #<JungleBeat:0x007fa7fa2ab508 @head=#<Node:0x007fa7fa2ab418 @data=nil, @link=#<Node:0x007fa7fa2ab3f0 @data="deep", @link=#<Node:0x007fa7fa2ab3c8 @data="boop", @link=#<Node:0x007fa7fa2ab3a0 @data="derp", @link=#<Node:0x007fa7fa2ab378 @data="burp", @link=nil>>>>>>
 
     assert_equal nil, jb.head.data                         # => true
     assert_equal "deep", jb.head.link.data                 # => true
@@ -29,18 +29,18 @@ class JungleBeatsTest < Minitest::Test
   end
 
   def test_can_find_head_with_one_string
-    jb = JungleBeat.new("deep")           # => #<JungleBeat:0x007fa2e290c310 @head=#<Node:0x007fa2e290c298 @data=nil, @link=#<Node:0x007fa2e290c270 @data="deep", @link=nil>>>
+    jb = JungleBeat.new("deep")           # => #<JungleBeat:0x007fa7fa2a2070 @head=#<Node:0x007fa7fa2a1ff8 @data=nil, @link=#<Node:0x007fa7fa2a1fd0 @data="deep", @link=nil>>>
 
-    jb.find_head  # => #<Node:0x007fa2e290c298 @data=nil, @link=#<Node:0x007fa2e290c270 @data="deep", @link=nil>>
+    jb.find_head  # => #<Node:0x007fa7fa2a1ff8 @data=nil, @link=#<Node:0x007fa7fa2a1fd0 @data="deep", @link=nil>>
 
     assert_equal nil, jb.head.data  # => true
     refute nil, jb.head.link        # => false
   end
 
   def test_can_find_head_with_multiple_string
-    jb = JungleBeat.new("deep boop")           # => #<JungleBeat:0x007fa2e290cce8 @head=#<Node:0x007fa2e290cc48 @data=nil, @link=#<Node:0x007fa2e290cc20 @data="deep", @link=#<Node:0x007fa2e290cbf8 @data="boop", @link=nil>>>>
+    jb = JungleBeat.new("deep boop")           # => #<JungleBeat:0x007fa7fa2985c0 @head=#<Node:0x007fa7fa298520 @data=nil, @link=#<Node:0x007fa7fa2984f8 @data="deep", @link=#<Node:0x007fa7fa2984d0 @data="boop", @link=nil>>>>
 
-    jb.find_head  # => #<Node:0x007fa2e290cc48 @data=nil, @link=#<Node:0x007fa2e290cc20 @data="deep", @link=#<Node:0x007fa2e290cbf8 @data="boop", @link=nil>>>
+    jb.find_head  # => #<Node:0x007fa7fa298520 @data=nil, @link=#<Node:0x007fa7fa2984f8 @data="deep", @link=#<Node:0x007fa7fa2984d0 @data="boop", @link=nil>>>
 
     assert_equal nil, jb.head.data  # => true
     refute nil, jb.head.link        # => false
@@ -53,21 +53,21 @@ class JungleBeatsTest < Minitest::Test
   end
 
   def test_can_find_tail_with_one_string
-    jb = JungleBeat.new("deep burp")      # => #<JungleBeat:0x007fa2e290d3c8 @head=#<Node:0x007fa2e290d328 @data=nil, @link=#<Node:0x007fa2e290d300 @data="deep", @link=#<Node:0x007fa2e290d2d8 @data="burp", @link=nil>>>>
+    jb = JungleBeat.new("deep burp")      # => #<JungleBeat:0x007fa7fa2a2750 @head=#<Node:0x007fa7fa2a26b0 @data=nil, @link=#<Node:0x007fa7fa2a2688 @data="deep", @link=#<Node:0x007fa7fa2a2660 @data="burp", @link=nil>>>>
 
     assert_equal nil, jb.find_tail.link  # => true
   end
 
   def test_can_append_single_beat_to_empty_beat
-    jb = JungleBeat.new                          # => #<JungleBeat:0x007fa2e290fab0 @head=nil>
-    jb.append("deep")                            # => #<Node:0x007fa2e290f920 @data="deep", @link=nil>
+    jb = JungleBeat.new                          # => #<JungleBeat:0x007fa7fa2923a0 @head=nil>
+    jb.append("deep")                            # => ["deep"]
 
     assert_equal "deep", jb.head.link.data  # => true
     assert_equal nil, jb.head.link.link     # => true
   end
 
   def test_can_append_single_beat_to_existing_beat
-    jb = JungleBeat.new("deep")                     # => #<JungleBeat:0x007fa2e2802668 @head=#<Node:0x007fa2e2802028 @data=nil, @link=#<Node:0x007fa2e2802000 @data="deep", @link=nil>>>
+    jb = JungleBeat.new("deep")                     # => #<JungleBeat:0x007fa7fa29a7a8 @head=#<Node:0x007fa7fa29a730 @data=nil, @link=#<Node:0x007fa7fa29a708 @data="deep", @link=nil>>>
     jb.append("burp")                               # => ["burp"]
 
     assert_equal "deep", jb.head.link.data       # => true
@@ -76,8 +76,18 @@ class JungleBeatsTest < Minitest::Test
     assert_equal nil, jb.head.link.link.link     # => true
   end
 
+  def test_can_append_multiple_beats_to_empty_beat
+    jb = JungleBeat.new                             # => #<JungleBeat:0x007fa7fa2b3a50 @head=nil>
+    jb.append("deep boop")                          # => ["deep", "boop"]
+
+    assert_equal "deep", jb.head.link.data       # => true
+    refute nil, jb.head.link.link                # => false
+    assert_equal "boop", jb.head.link.link.data  # => true
+    assert_equal nil, jb.head.link.link.link     # => true
+  end
+
   def test_can_append_multiple_beats_to_existing_beat
-    jb = JungleBeat.new("deep")                        # => #<JungleBeat:0x007fa2e290f2e0 @head=#<Node:0x007fa2e290f268 @data=nil, @link=#<Node:0x007fa2e290f240 @data="deep", @link=nil>>>
+    jb = JungleBeat.new("deep")                        # => #<JungleBeat:0x007fa7fa299c18 @head=#<Node:0x007fa7fa299ba0 @data=nil, @link=#<Node:0x007fa7fa299b78 @data="deep", @link=nil>>>
     jb.append("burp boop")                             # => ["burp", "boop"]
 
     assert_equal "burp", jb.head.link.link.data       # => true
@@ -85,14 +95,167 @@ class JungleBeatsTest < Minitest::Test
     assert_equal "boop", jb.head.link.link.link.data  # => true
     assert_equal nil, jb.head.link.link.link.link     # => true
   end
+
+  def test_can_prepend_to_no_beats
+    jb = JungleBeat.new             # => #<JungleBeat:0x007fa7fa2a9cd0 @head=nil>
+    jb.prepend("deep")              # => ["deep"]
+
+    assert_equal "deep", jb.head.link.data  # => true
+    assert_equal nil, jb.head.link.link     # => true
+  end
+
+  def test_can_prepend_to_one_beat
+    jb = JungleBeat.new("deep")     # => #<JungleBeat:0x007fa7fa82de18 @head=#<Node:0x007fa7fa82dc60 @data=nil, @link=#<Node:0x007fa7fa82dc38 @data="deep", @link=nil>>>
+    jb.prepend("boop")              # => #<Node:0x007fa7fa82dc38 @data="deep", @link=nil>
+    jb.head.link                    # => #<Node:0x007fa7fa82c1f8 @data="boop", @link=#<Node:0x007fa7fa82dc38 @data="deep", @link=nil>>
+
+    assert_equal "boop", jb.head.link.data       # => true
+    refute nil, jb.head.link.link                # => false
+    assert_equal "deep", jb.head.link.link.data  # => true
+    assert_equal nil, jb.head.link.link.link     # => true
+  end
+
+  def test_can_prepend_to_multiple_beats
+    jb = JungleBeat.new("deep boop")      # => #<JungleBeat:0x007fa7fa2a0478 @head=#<Node:0x007fa7fa2a03d8 @data=nil, @link=#<Node:0x007fa7fa2a03b0 @data="deep", @link=#<Node:0x007fa7fa2a0388 @data="boop", @link=nil>>>>
+    jb.prepend("blap borp")               # => #<Node:0x007fa7fa2a03b0 @data="deep", @link=#<Node:0x007fa7fa2a0388 @data="boop", @link=nil>>
+    jb.head.link                          # => #<Node:0x007fa7fa2a0018 @data="blap", @link=#<Node:0x007fa7fa827db0 @data="borp", @link=#<Node:0x007fa7fa2a03b0 @data="deep", @link=#<Node:0x007fa7fa2a0388 @data="boop", @link=nil>>>>
+
+    assert_equal "blap", jb.head.link.data                 # => true
+    refute nil, jb.head.link.link                          # => false
+    assert_equal "borp", jb.head.link.link.data            # => true
+    refute nil, jb.head.link.link.link                     # => false
+    assert_equal "deep", jb.head.link.link.link.data       # => true
+    refute nil, jb.head.link.link.link.link                # => false
+    assert_equal "boop", jb.head.link.link.link.link.data  # => true
+    assert_equal nil, jb.head.link.link.link.link.link     # => true
+  end
+
+  def test_can_insert_one_beat_into_empty_list
+    skip
+    jb = JungleBeat.new
+    jb.insert(0, "tee")
+
+    assert_equal "tee", jb.head.link.data
+  end
+
+  def test_can_insert_single_beat_into_list
+    jb = JungleBeat.new("deep boop")         # => #<JungleBeat:0x007fa7fa2a90a0 @head=#<Node:0x007fa7fa2a9000 @data=nil, @link=#<Node:0x007fa7fa2a8fd8 @data="deep", @link=#<Node:0x007fa7fa2a8fb0 @data="boop", @link=nil>>>>
+    jb.insert(1, "tee")                      # => #<Node:0x007fa7fa2a8fb0 @data="boop", @link=nil>
+
+    assert_equal "deep", jb.head.link.data            # => true
+    refute nil, jb.head.link.link                     # => false
+    assert_equal "tee", jb.head.link.link.data        # => true
+    refute nil, jb.head.link.link.link                # => false
+    assert_equal "boop", jb.head.link.link.link.data  # => true
+    assert_equal nil, jb.head.link.link.link.link     # => true
+  end
+
+  def test_can_insert_multiple_beats_into_list
+    jb = JungleBeat.new("deep tee boop do")     # => #<JungleBeat:0x007fa7fa291bd0 @head=#<Node:0x007fa7fa291ae0 @data=nil, @link=#<Node:0x007fa7fa291ab8 @data="deep", @link=#<Node:0x007fa7fa291a90 @data="tee", @link=#<Node:0x007fa7fa291a68 @data="boop", @link=#<Node:0x007fa7fa291a40 @data="do", @link=nil>>>>>>
+    jb.insert(2, "blap durp")                   # => #<Node:0x007fa7fa291a68 @data="boop", @link=#<Node:0x007fa7fa291a40 @data="do", @link=nil>>
+
+    assert_equal "deep", jb.head.link.data                         # => true
+    refute nil, jb.head.link.link                                  # => false
+    assert_equal "tee", jb.head.link.link.data                     # => true
+    refute nil, jb.head.link.link.link                             # => false
+    assert_equal "blap", jb.head.link.link.link.data               # => true
+    refute nil, jb.head.link.link.link.link                        # => false
+    assert_equal "durp", jb.head.link.link.link.link.data          # => true
+    refute nil, jb.head.link.link.link.link.link                   # => false
+    assert_equal "boop", jb.head.link.link.link.link.link.data     # => true
+    refute nil, jb.head.link.link.link.link.link.link              # => false
+    assert_equal "do", jb.head.link.link.link.link.link.link.data  # => true
+    assert_equal nil, jb.head.link.link.link.link.link.link.link   # => true
+  end
+
+  def test_can_find_multiple_beats_in_list
+    jb = JungleBeat.new("deep durp burp tee boop")  # => #<JungleBeat:0x007fa7fa299010 @head=#<Node:0x007fa7fa298ef8 @data=nil, @link=#<Node:0x007fa7fa298ed0 @data="deep", @link=#<Node:0x007fa7fa298ea8 @data="durp", @link=#<Node:0x007fa7fa298e80 @data="burp", @link=#<Node:0x007fa7fa298e58 @data="tee", @link=#<Node:0x007fa7fa298e30 @data="boop", @link=nil>>>>>>>
+
+    assert_equal "burp tee boop", jb.find(2, 3)  # => true
+  end
+
+  def test_can_find_single_beat_in_list
+    jb = JungleBeat.new("deep durp burp tee boop")  # => #<JungleBeat:0x007fa7fa29bec8 @head=#<Node:0x007fa7fa29bdb0 @data=nil, @link=#<Node:0x007fa7fa29bd88 @data="deep", @link=#<Node:0x007fa7fa29bd60 @data="durp", @link=#<Node:0x007fa7fa29bd38 @data="burp", @link=#<Node:0x007fa7fa29bd10 @data="tee", @link=#<Node:0x007fa7fa29bce8 @data="boop", @link=nil>>>>>>>
+
+    assert_equal "burp", jb.find(2,1)  # => true
+  end
+
+  def test_can_tell_if_given_beat_is_in_list
+    jb = JungleBeat.new("deep durp burp tee boop")  # => #<JungleBeat:0x007fa7fa2939a8 @head=#<Node:0x007fa7fa293890 @data=nil, @link=#<Node:0x007fa7fa293868 @data="deep", @link=#<Node:0x007fa7fa293840 @data="durp", @link=#<Node:0x007fa7fa293818 @data="burp", @link=#<Node:0x007fa7fa2937f0 @data="tee", @link=#<Node:0x007fa7fa2937c8 @data="boop", @link=nil>>>>>>>
+
+    assert_equal true, jb.includes?("burp")  # => true
+    assert_equal true, jb.includes?("boop")  # => true
+  end
+
+  def test_can_count_number_of_beats_in_list
+    jb = JungleBeat.new("deep durp burp tee boop")  # => #<JungleBeat:0x007fa7fa2aa658 @head=#<Node:0x007fa7fa2aa540 @data=nil, @link=#<Node:0x007fa7fa2aa518 @data="deep", @link=#<Node:0x007fa7fa2aa4f0 @data="durp", @link=#<Node:0x007fa7fa2aa4c8 @data="burp", @link=#<Node:0x007fa7fa2aa4a0 @data="tee", @link=#<Node:0x007fa7fa2aa478 @data="boop", @link=nil>>>>>>>
+
+    assert_equal 5, jb.count  # => true
+  end
+
+  def test_can_count_number_of_beats_in_single_beat_list
+    jb = JungleBeat.new("deep")                           # => #<JungleBeat:0x007fa7fa2a3588 @head=#<Node:0x007fa7fa2a3510 @data=nil, @link=#<Node:0x007fa7fa2a34e8 @data="deep", @link=nil>>>
+
+    assert_equal 1, jb.count  # => true
+  end
+
+  def test_can_count_number_of_beats_in_no_beat_list
+    jb = JungleBeat.new                               # => #<JungleBeat:0x007fa7fa2a9528 @head=nil>
+
+    assert_equal 0, jb.count  # => true
+  end
+
+  def test_can_pop_beats_off_the_list
+    jb = JungleBeat.new("deep durp burp tee boop")  # => #<JungleBeat:0x007fa7fa292e90 @head=#<Node:0x007fa7fa292d78 @data=nil, @link=#<Node:0x007fa7fa292d50 @data="deep", @link=#<Node:0x007fa7fa292d28 @data="durp", @link=#<Node:0x007fa7fa292d00 @data="burp", @link=#<Node:0x007fa7fa292cd8 @data="tee", @link=#<Node:0x007fa7fa292cb0 @data="boop", @link=nil>>>>>>>
+    assert_equal "tee boop", jb.pop(2)              # => true
+  end
+
+  def test_cannot_pop_beats_off_empty_list
+    jb = JungleBeat.new                     # => #<JungleBeat:0x007fa7fa29b4a0 @head=nil>
+
+    assert_equal "Cannot pop off any beats!", jb.pop(1)  # => true
+  end
+
+  def test_can_output_all_beats_from_list
+    jb = JungleBeat.new("deep durp burp tee boop")  # => #<JungleBeat:0x007fa7fa290370 @head=#<Node:0x007fa7fa290258 @data=nil, @link=#<Node:0x007fa7fa290230 @data="deep", @link=#<Node:0x007fa7fa290208 @data="durp", @link=#<Node:0x007fa7fa2901e0 @data="burp", @link=#<Node:0x007fa7fa2901b8 @data="tee", @link=#<Node:0x007fa7fa290190 @data="boop", @link=nil>>>>>>>
+
+    assert_equal "deep durp burp tee boop", jb.all  # => true
+  end
+
+  def test_can_output_beat_from_single_beat_list
+    jb = JungleBeat.new("deep")                   # => #<JungleBeat:0x007fa7fa29af50 @head=#<Node:0x007fa7fa29aed8 @data=nil, @link=#<Node:0x007fa7fa29aeb0 @data="deep", @link=nil>>>
+
+    assert_equal "deep", jb.all  # => true
+  end
+
+  def test_can_play
+    jb = JungleBeat.new("ding dah oom oom ding oom oom oom ding dah oom oom ding dah oom oom ding")  # => #<JungleBeat:0x007fa7fa2a17d8 @head=#<Node:0x007fa7fa2a14e0 @data=nil, @link=#<Node:0x007fa7fa2a14b8 @data="ding", @link=#<Node:0x007fa7fa2a1490 @data="dah", @link=#<Node:0x007fa7fa2a1468 @data="oom", @link=#<Node:0x007fa7fa2a1440 @data="oom", @link=#<Node:0x007fa7fa2a1418 @data="ding", @link=#<Node:0x007fa7fa2a13f0 @data="oom", @link=#<Node:0x007fa7fa2a13c8 @data="oom", @link=#<Node:0x007fa7fa2a13a0 @data="oom", @link=#<Node:0x007fa7fa2a1378 @data="ding", @link=#<Node:0x007fa7fa2a1350 @data="dah", @link=#<Node:0x007fa7fa2a1328 @data="oom", @link=#<Node:0x007fa7fa2a1300 @data="oom", @link=#<Node:0x007fa7fa2a12d8 @data="ding", @link=#<Node:0x007fa7fa2a12b0 @data="dah", @link=#<Node:0x007fa7fa2a1288 @data="oom", @link=#<Node:0x007fa7fa2a1260 @data="oom", @link=#<Node:0x007fa7fa2a1238 @data="ding", @link=nil>>>>>>>>>>>>>>>>>>>
+
+    jb.play  # => "ding dah oom oom ding oom oom oom ding dah oom oom ding dah oom oom ding"
+  end
 end
 
-# >> Run options: --seed 17083
+# >> Run options: --seed 63287
 # >>
 # >> # Running:
 # >>
-# >> .........
+# >> .5
+# >> "tee boop"
+# >> ...5
+# >> "deep durp burp tee boop"
+# >> ."burp"
+# >> ."Cannot pop off any beats!"
+# >> .1
+# >> "deep"
+# >> ..."burp tee boop"
+# >> ...1
+# >> ....17
+# >> "ding dah oom oom ding oom oom oom ding dah oom oom ding dah oom oom ding"
+# >> ...5
+# >> .....S.
 # >>
-# >> Finished in 0.001312s, 6859.4372 runs/s, 17529.6728 assertions/s.
+# >> Finished in 0.002209s, 12223.8264 runs/s, 31691.4018 assertions/s.
 # >>
-# >> 9 runs, 23 assertions, 0 failures, 0 errors, 0 skips
+# >> 27 runs, 70 assertions, 0 failures, 0 errors, 1 skips
+# >>
+# >> You have skipped tests. Run with --verbose for details.
